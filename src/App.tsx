5f459166cca788721854c65e092354a7918752a5
cxy0820 <ncheng820@gmail.com>
2026-06-11 22:23:51 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -711,12 +711,12 @@ function DailyPoster({ records, urls }: { records: FoodRecord[]; urls: Record<st
     await drawPosterSticker(ctx, "/poster-decor/flower.svg", 800, 370, 110, -9, 12);
     await drawPosterSticker(ctx, "/poster-decor/cake.svg", 160, 930, 112, -4, 12);
     await drawPosterSticker(ctx, "/poster-decor/glowing-star.svg", 840, 930, 108, 8, 12);
-    await drawPosterSticker(ctx, urls[hero.stickerImageId], 540, 580, 480, 5);
-    if (second) await drawPosterSticker(ctx, urls[second.stickerImageId], 170, 545, 190, -10);
-    if (third) await drawPosterSticker(ctx, urls[third.stickerImageId], 910, 535, 190, 11);
-    drawPosterLabel(ctx, dailyPosterQuotes[0], 122, 820, "#efcbd8", 7);
+    await drawPosterSticker(ctx, urls[hero.stickerImageId], 540, 580, 560, 5);
+    if (second) await drawPosterSticker(ctx, urls[second.stickerImageId], 160, 665, 190, -10);
+    if (third) await drawPosterSticker(ctx, urls[third.stickerImageId], 920, 640, 190, 11);
+    drawPosterLabel(ctx, dailyPosterQuotes[0], 98, 805, "#efcbd8", 7);
     drawPosterLabel(ctx, dailyPosterQuotes[1], 720, 880, "#dcd3f0", -6);
-    drawPosterLabel(ctx, dailyPosterQuotes[2], 390, 1000, "#ffe391", 3);
+    drawPosterLabel(ctx, dailyPosterQuotes[2], 150, 1070, "#ffe391", 3);
 
     ctx.save();
     ctx.translate(804, 1124);
